6b87e4603019d109e8daaa4a11f83d989f08e018
taikoumuseen <taikoumuseen@gmail.com>
2025-03-06 09:47:32 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,8 @@
+  public class MathProblems {
+    public static void main(String[] args) {
+      int x = (int)(Math.random() * 10);
+      int y = (int)(Math.random() * 10);
+      int sum = x + y;
+      System.out.println("The sum of " + x + " and " + y + " is " + sum);
+    }
+}
