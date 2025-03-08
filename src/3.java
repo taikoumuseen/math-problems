b3587289efd082a6c2ed7ce29653f9d12b1ec0ca
taikoumuseen <taikoumuseen@gmail.com>
2025-03-08 12:04:59 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,9 @@
+
+import java.util.Random;
+public class MathProblems {
+    public static int getRandomNumber(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+}
