b9bb8ecb87eef8e680f8c42abac40361370969a3
taikoumuseen <taikoumuseen@gmail.com>
2025-03-11 11:35:12 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class MathProblem {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int a = r.nextInt(10);
+        int b = r.nextInt(10);
+        System.out.println(a + " + " + b + " = ");
+    }
+}
