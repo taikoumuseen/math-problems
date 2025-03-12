cf5a5d75c8f1e990954d6465da285de6b6abc272
taikoumuseen <taikoumuseen@gmail.com>
2025-03-12 15:12:09 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,9 @@
+// Java program to find the sum of two numbers
+
+class Sum{
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 8;
+    System.out.println("The sum is " + (num1+num2));
+  }
+}
