78d2937078d73b60232c586368a5e7d7c09d0c0d
taikoumuseen <taikoumuseen@gmail.com>
2025-03-14 21:49:17 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MathProblems {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    System.out.println(num1 + num2);
+  }
+}
