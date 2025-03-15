f485f08fdbef8bf8a6b77a5d64322d1a24b15f6e
taikoumuseen <taikoumuseen@gmail.com>
2025-03-15 11:09:50 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = new Random().nextInt(10);
+        System.out.println("What is " + x + " times 3?");
+    }
+}
