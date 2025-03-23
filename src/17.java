8cdb0a06bd9089bbe1fcd3d15784a74ad891cbf4
taikoumuseen <taikoumuseen@gmail.com>
2025-03-23 15:52:02 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,14 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example problem 1: Find the area of a rectangle
+        double length = 5.0;
+        double width = 3.0;
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+
+        // Example problem 2: Calculate the perimeter of an equilateral triangle
+        double sideLength = 4.0;
+        double perimeter = 3 * sideLength;
+        System.out.println("The perimeter of the equilateral triangle is: " + perimeter);
+    }
+}
