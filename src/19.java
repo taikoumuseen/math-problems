3e0a75eabd849ecff2b70bad2ec3e382941bcfe5
taikoumuseen <taikoumuseen@gmail.com>
2025-03-29 19:25:03 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,17 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Simple addition
+        System.out.println("Result of 2 + 3 = " + (2 + 3));
+
+        // Example 2: Multiplication of two numbers
+        double result = 5 * 7;
+        System.out.println("The result of multiplication is: " + result);
+
+        // Example 3: Addition with a string and integer
+        String strNumber = "42";
+        int number = 12;
+        double resultString = (double) strNumber + number;
+
+        System.out.println("Result of adding string '42' and 12 is: " + resultString);
+    }
+}
