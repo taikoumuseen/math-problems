e8b405b3dd4511a55947cdebdddcbc4a0c8296b6
taikoumuseen <taikoumuseen@gmail.com>
2025-04-01 16:02:19 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        double z = (x + y) * 2;
+
+        System.out.println("The sum of " + x + " and " + y + " is: " + z);
+    }
+}
