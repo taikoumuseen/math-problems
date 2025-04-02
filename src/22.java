0f42f78abdaffc234a7c906eb15a23908379a993
taikoumuseen <taikoumuseen@gmail.com>
2025-04-02 16:19:33 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = 3.0;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + (num1 - num2));
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + (num1 * num2));
+        System.out.println("The quotient of " + num1 + " and " + num2 + " is: " + (double)(num1 / num2));
+    }
+}
