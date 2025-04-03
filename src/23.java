b32c21bcfe4719aae15f65e406ff13a08fd8065a
taikoumuseen <taikoumuseen@gmail.com>
2025-04-03 20:49:05 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,12 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example of solving simple arithmetic problem: 5 + 3 = 8
+
+        int result = 5 + 3;
+        System.out.println("The sum is " + result);
+        
+        // Another example: 10 - 2 * 4 / 2
+        int subtraction = 10 - (2 * 4) / 2;
+        System.out.println("Subtraction result: " + subtraction);
+    }
+}
