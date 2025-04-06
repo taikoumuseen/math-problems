5813373bb71fe8c5853504eb5809a0d1805b3afc
taikoumuseen <taikoumuseen@gmail.com>
2025-04-06 06:30:17 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,12 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        // Example of solving basic arithmetic operations
+        System.out.println("Result: 5 + 3 = " + (5 + 3));
+        System.out.println("Result: 10 - 2 * 4 = " + (10 - 2 * 4));
+        System.out.println("Result: 7 / 2 = " + (7 / 2));
+
+        // Example of using arithmetic operations to solve a quadratic equation
+        double x = Math.sqrt(25);
+        System.out.println("Roots of the equation x^2 - 5x + 6 = 0 are: " + x);
+    }
+}
