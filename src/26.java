720d2dc85e56cd43f7156faf5638c120a2338d51
taikoumuseen <taikoumuseen@gmail.com>
2025-04-07 17:48:34 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
