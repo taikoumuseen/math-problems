f0da86ad0e4275361be982852247df604a84fcc8
taikoumuseen <taikoumuseen@gmail.com>
2025-04-08 22:46:15 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,12 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        double result = add(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+
+    public static double add(int a, int b) {
+        return a + b;
+    }
+}
