53d9753c6d699a15339b3045284410bee711db47
taikoumuseen <taikoumuseen@gmail.com>
2025-04-11 11:42:38 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,6 @@
+public class MathSolver {
+    public static int solveProblem(int a, int b) {
+        // Add your code here to solve the problem
+        return 0; // Replace with actual solution
+    }
+}
