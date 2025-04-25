22e5400a651f40d895362a1eafa74e2cc9a8eea4
taikoumuseen <taikoumuseen@gmail.com>
2025-04-25 12:27:35 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,10 @@
+public class MathProblemsSolver {
+    public static void main(String[] args) {
+        // Example mathematical problem: find the area of a triangle
+        double base = 5.0;
+        double height = 3.0;
+        double area = 0.5 * base * height;
+
+        System.out.println("The area of the triangle is: " + area);
+    }
+}
