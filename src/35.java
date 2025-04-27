db785f58a10f150e42e036f6d48c79c66c1cf2f5
taikoumuseen <taikoumuseen@gmail.com>
2025-04-27 12:06:36 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,8 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        double result = (num1 + num2) * 4.0;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
