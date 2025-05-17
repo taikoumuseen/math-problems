d23c67daf59ca7b2608bd11bababd8cc2a324476
taikoumuseen <taikoumuseen@gmail.com>
2025-05-17 09:05:33 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+        System.out.println("The difference between " + a + " and " + b + " is: " + (a - b));
+    }
+}
