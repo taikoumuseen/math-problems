0af7c4f7901780382c5f17dc33f666e0624a5bad
taikoumuseen <taikoumuseen@gmail.com>
2025-05-18 11:47:24 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,23 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code: simple addition and subtraction
+        System.out.println("Example 1:");
+        int a = 5;
+        int b = 3;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+
+        // Additional examples:
+        System.out.println("\nExample 2:");
+        int c = 8;
+        int d = 4;
+        System.out.println("Addition: " + (c + d));
+        System.out.println("Subtraction: " + (c - d));
+
+        System.out.println("\nExample 3:");
+        int e = 10;
+        int f = 2;
+        System.out.println("Addition: " + (e + f));
+        System.out.println("Subtraction: " + (e - f));
+    }
+}
