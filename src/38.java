02e8d7a43302ff22f716a8d6f1175341414887a7
taikoumuseen <taikoumuseen@gmail.com>
2025-05-18 10:37:14 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,9 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
