7e655d493b6f1e28629e347455aa21776c684a9f
taikoumuseen <taikoumuseen@gmail.com>
2025-05-23 07:20:06 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,8 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 3;
+        double result = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+    }
+}
