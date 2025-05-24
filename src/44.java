91530645da6c24eebc1c239301b00ab5bef2ac26
taikoumuseen <taikoumuseen@gmail.com>
2025-05-24 20:09:09 +0000

    Add 44.java

---
+++ src/44.java
@@ -0,0 +1,10 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+        System.out.println("The difference between " + a + " and " + b + " is: " + (a - b));
+        System.out.println("The product of " + a + " and " + b + " is: " + (a * b));
+    }
+}
