f36f3e6483434832b7bdcef590a01ce5da019fb6
taikoumuseen <taikoumuseen@gmail.com>
2025-05-25 06:07:07 +0000

    Add 46.java

---
+++ src/46.java
@@ -0,0 +1,9 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        double num1 = 3.5;
+        double num2 = -2.0;
+        
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and "-+num2+" is: "+result);
+    }
+}
