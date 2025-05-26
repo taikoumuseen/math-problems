576ec10ea85ad0b4ce0c5f125da1e64488678a4d
taikoumuseen <taikoumuseen@gmail.com>
2025-05-26 20:47:48 +0000

    Add 47.java

---
+++ src/47.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int num1 = 5;
+        int num2 = 3;
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
